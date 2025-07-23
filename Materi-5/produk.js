cfc8141b482a04c8bdf82a4e2e9420c15e6f0656
Uryan992 <yannt279@gmail.com>
2025-07-23 22:57:42 +0800

    Add files via upload

---
+++ Materi-5/produk.js
@@ -0,0 +1,44 @@
+const products = [
+  {
+    name: "Adidas Adizero Evo SL",
+    description: "Nyaman untuk jalan santai dan olahraga.",
+    price: 3100000,
+    image: "https://senikersku.com/wp-content/uploads/2024/12/JP7149-1.png",
+    category: "Fashion"
+  },
+  {
+    name: "Jersey Barca 25/26",
+    description: "Football Jersey.",
+    price: 300000,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQj4z_U2AMH_xddYykRQjl1w1WNBypSFzismw&s",
+    category: "Fashion"
+  },
+  {
+    name: "Gaming chair",
+    description: "Ergonomis, cocok untuk kerja seharian.",
+    price: 850000,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRh12bHZ7fSO9mWpBsQyvRj-eJkRP_L7DHjw&s",
+    category: "Perabot"
+  },
+  {
+    name: "ROG Strix Scar 18",
+    description: "Gaming Laptop.",
+    price: 66000000,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQukx2ZcPppljIUb0mZyQiI8p8qeu8Tupg6A&s",
+    category: "Elektronik"
+  },
+  {
+    name: "IPhone 16 Pro Max",
+    description: "Murah cocok untuk mahasiswa.",
+    price: 22999000,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK8RiaLJRnGNrF6_harzeTe5zBL-06-dDlYQ&s",
+    category: "Elektronik"
+  },
+  {
+    name: "Xiaomi Mi TV 43",      
+    description: "Smart TV.",
+    price: 3999000,
+    image: "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//catalog-image/114/MTA-141649989/xiaomi_xiaomi_mi_tv_43-_a2_l43m7_-_uhd_wifi_smart_tv_android_10_-_digital_full02_mj5jjovr.jpg",
+    category: "Elektronik"
+  }
+];
